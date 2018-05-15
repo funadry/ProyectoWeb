15a6ea72b55403a77940a09b888d1eb7a69dd8cd
funadry <36886989+funadry@users.noreply.github.com>
2018-05-15 17:37:50 +0200

    Menu, y mapeo

---
+++ WebShop/src/main/java/com/mavenproject/modelo/Libro.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mavenproject.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Tfg
+ */
+@Entity
+@Table(name="libro")
+public class Libro implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="titulo")
+    private String Titulo;
+
+    @Column(name="precio")
+    private Double Precio;
+
+    @Column(name="sinopsis")
+    private String Sinopsis;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double Precio) {
+        this.Precio = Precio;
+    }
+
+    public String getSinopsis() {
+        return Sinopsis;
+    }
+
+    public void setSinopsis(String Sinopsis) {
+        this.Sinopsis = Sinopsis;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "id=" + id + ", Titulo=" + Titulo + ", Precio=" + Precio + ", Sinopsis=" + Sinopsis + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Libro other = (Libro) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
